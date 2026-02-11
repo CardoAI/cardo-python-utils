801f943dd44f6e69a2380dd11f42dbed9cba348e
Klajdi Çaushi <klajdi.caushi@cardoai.com>
2026-02-11 14:11:46 +0100

    fix(celery): do not import TenantAwareDatabaseScheduler

--- python_utils/django/celery/__init__.py
+++ python_utils/django/celery/__init__.py
@@ -1,5 +1,4 @@
 from .tenant_aware_task import TenantAwareTask
-from .tenant_aware_database_scheduler import TenantAwareDatabaseScheduler
 
 
-__all__ = ["TenantAwareTask", "TenantAwareDatabaseScheduler"]
+__all__ = ["TenantAwareTask"]
