68fe2553d2beaca7d8e999411631d1ebbfff774d
Klajdi Çaushi <klajdi.caushi@cardoai.com>
2026-02-12 10:45:37 +0100

    feat(django): added user model

---
+++ python_utils/django/models/user.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+
+class User(AbstractUser):
+    is_demo = models.BooleanField(default=False, null=False, help_text="Whether this user is a demo user.")
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name="groups",
+        blank=True,
+        help_text=("The groups this user belongs to. A user will get all permissions granted to each of their groups."),
+        related_name="idp_user_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name="user permissions",
+        blank=True,
+        help_text="Specific permissions for this user.",
+        related_name="idp_user_set",
+        related_query_name="user",
+    )
+
+    class Meta:
+        db_table = "idp_user_user"
